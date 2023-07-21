0cc552530cf25454141ffdff139bfe9bf9f515ce
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-07-21 15:36:21 +0200

    feat: onmessage function

--- ChatApp/ChatApp/asgi.py
+++ ChatApp/ChatApp/asgi.py
@@ -13,8 +13,8 @@ from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 import chatroom.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
